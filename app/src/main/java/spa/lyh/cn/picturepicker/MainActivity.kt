573d44fb767425211febd20c31c46ba39bb4d3bf
liyuhao <liyuhaoid@sina.com>
2024-09-26 22:04:18 +0800

    Initial commit

---
+++ app/src/main/java/spa/lyh/cn/picturepicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package spa.lyh.cn.picturepicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
